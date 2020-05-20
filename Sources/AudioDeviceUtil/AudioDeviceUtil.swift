9117c9ea2f0125b7a7c1e714d8f9c3b9dc6f00b8
Boring Wozniak <real.boring.wozniak@gmail.com>
2020-05-20 15:13:08 +0200

    Added function getPropertyDataSize

--- Sources/AudioDeviceUtil/AudioDeviceUtil.swift
+++ Sources/AudioDeviceUtil/AudioDeviceUtil.swift
@@ -7,3 +7,17 @@ let DefaultElement = kAudioObjectPropertyElementMaster
 func toPropertyAddress(selector: AudioObjectPropertySelector) -> AudioObjectPropertyAddress {
     AudioObjectPropertyAddress(mSelector: selector, mScope: DefaultScope, mElement: DefaultElement)
 }
+
+func getPropertyDataSize(objectID: AudioObjectID, address: AudioObjectPropertyAddress) -> (size: Int?, status: OSStatus) {
+    var result: UInt32 = 0
+
+    var addressVar = address // MUST be `var` so it can be passed as an `in-out` parameter
+    let status = AudioObjectGetPropertyDataSize(objectID, &addressVar, 0, nil, &result)
+
+    return (status == noErr ? Int(result) : nil, status)
+}
+
+// Convenience method for calls like `getPropertyDataSize(objectID: kAudioObjectSystemObject, selector: kAudioHardwarePropertyDevices)`
+func getPropertyDataSize(objectID: Int32, selector: AudioObjectPropertySelector) -> (size: Int?, status: OSStatus) {
+    getPropertyDataSize(objectID: AudioObjectID(objectID), address: toPropertyAddress(selector: selector))
+}
--- Tests/AudioDeviceUtilTests/AudioDeviceUtilTests.swift
+++ Tests/AudioDeviceUtilTests/AudioDeviceUtilTests.swift
@@ -4,15 +4,26 @@ import XCTest
 
 final class AudioDeviceUtilTests: XCTestCase {
     func testToPropertyAddress() {
-        let selector = AudioObjectPropertySelector(kAudioObjectSystemObject)
+        let selector = kAudioHardwarePropertyDevices
         let result = toPropertyAddress(selector: selector)
 
         XCTAssertEqual(result.mSelector, selector)
         XCTAssertEqual(result.mScope, DefaultScope)
         XCTAssertEqual(result.mElement, DefaultElement)
     }
+    
+    func testGetPropertyDataSize() {
+        let result = getPropertyDataSize(
+            objectID: kAudioObjectSystemObject,
+            selector: kAudioHardwarePropertyDevices
+        )
+
+        XCTAssertEqual(result.status, noErr)
+        XCTAssertNotNil(result.size)
+    }
 
     static var allTests = [
         ("testToPropertyAddress", testToPropertyAddress),
+        ("testGetPropertyDataSize", testGetPropertyDataSize),
     ]
 }
